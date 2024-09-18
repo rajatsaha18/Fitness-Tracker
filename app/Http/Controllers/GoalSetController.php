5f53b694440ac671fdd48c1a3f4197619d8dfa31
rajatsaha18 <editorrajatdiu@gmail.com>
2024-09-18 10:23:48 +0600

    update: model and migration in Goal

--- app/Http/Controllers/GoalSetController.php
+++ app/Http/Controllers/GoalSetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Goal;
 use Illuminate\Http\Request;
 
 class GoalSetController extends Controller
@@ -9,5 +10,15 @@ class GoalSetController extends Controller
     public function index()
     {
         return view('website.goal.index');
+    }
+    public function newGoal(Request $request)
+    {
+        $request->validate([
+            'description'   => 'required',
+            'start_date'    => 'required|date',
+            'end_date'      => 'required|date',
+        ]);
+        Goal::newGoalSet($request);
+        return redirect()->back();
     }
 }
